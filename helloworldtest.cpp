02ebe988ba0ac83feeef53f5df080f176149e8e0
chiayen <38468171+yen2451@users.noreply.github.com>
2018-08-19 20:17:16 +0800

    add vscode cpp setup

---
+++ helloworldtest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    cout<<"hello world!!!"<<endl;
+
+
+    system ("pause");
+    return 0;
+}
