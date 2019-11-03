faad4811bd456a9c147e7a2c4aec14bcb4cfebfd
chiayen <38468171+yen2451@users.noreply.github.com>
2019-11-03 16:35:27 +0800

    zj_d635

---
+++ zj_d635.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//§@ªÌ:11418
+int main()
+{
+  int n;
+  
+  while(cin>>n){
+  	if(n<0)cout<<"-1"<<endl;
+  	else cout<<oct<<n<<endl;
+  }
+  	
+
+  return 0;
+}
